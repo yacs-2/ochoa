556764af58858c2be39cd34fa12216e58b158a2a
yacs-2 <chromebook2@youngsvilleacademy.org>
2025-05-19 08:33:08 -0400

    Update pythag.js

--- wdsub/pythag.js
+++ wdsub/pythag.js
@@ -4,15 +4,17 @@ function compute() {
   let _b = _inputArr[1].value;
   let _c = _inputArr[2].value;
   
-  if ( _a = "" && _b = " " && _c = " ") {
+  if (_a == "" && _b != "" && _c != "") { _inputArr[0].value = Math.sqrt(Math.pow(_c, 2) - Math.pow(_b, 2));
     
-    _a = _c**2 - _b**2;
+  } else if (_a != "" && _b == "" && _c != "") { 
+   _inputArr[1].value = Math.sqrt(Math.pow(_c, 2) - Math.pow(_a, 2));
     
-  } else if ( _a = " " && _b = "" && _c = " ") {
-    
-    _b = _c**2 - _a**2;
+  } else if (_a != "" && _b != "" && _c == "") { 
+    _inputArr[2].value = Math.sqrt(Math.pow(_a, 2) + Math.pow(_b, 2));
+  } else { 
+    window.alert("Please enter two side lengths");
+  }
   
-  } else if ( _a = " " && _b = "" && _c = " ") {}
   
   
 }
